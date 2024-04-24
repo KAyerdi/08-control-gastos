6207e4a8619b90bc2956277091b59880c19c503c
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-04-24 02:15:19 -0300

    Agregando useState y validando form con useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BudgetForm from "./components/BudgetForm"
 
 
 function App() {
@@ -5,7 +6,14 @@ function App() {
 
   return (
     <>
-      <h1 className="">Control de Gastos</h1>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+          </h1>
+      </header>
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <BudgetForm />
+      </div>
     </>
   )
 }
