f4573c0e7f13c5e973f66fd7fb96ee0d338490f1
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-05-04 05:39:17 -0300

    finalizando proyecto

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -7,10 +7,9 @@ export default function ExpenseList() {
   
   const { state } = useBudget()
 
-  const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses])
-
   const filteredExpenses = state.currentCategory ? state.expenses.filter( expense => expense.category === state.currentCategory) : state.expenses
 
+  const isEmpty = useMemo(() => filteredExpenses.length === 0, [filteredExpenses])
 
   return(
     <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
